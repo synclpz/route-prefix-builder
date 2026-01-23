14d1c3dfc03d966827e91dd859dc326afcd046fb
Виктор Александров <v.aleksandrov@consaltica.ru>
2026-01-23 22:15:49 +0300

    Changed build/run methods

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
-pipenv install
-nohup pipenv run python3 route-prefix-builder.py >/dev/null 2>&1 &
-
+#!/bin/bash
+cd $1
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt >/dev/null 2>&1
+nohup python3 route-prefix-builder.py >/dev/null 2>&1 &
+deactivate
